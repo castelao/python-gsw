5afdfa378c5370633f7d2fbd238f90d8a569990a
Guilherme Castelão <guilherme@castelao.net>
2017-03-02 14:07:03 -0800

    density_enthalpy_75.alpha_on_beta() from library76

--- gsw/gibbs/density_enthalpy_75.py
+++ gsw/gibbs/density_enthalpy_75.py
@@ -235,6 +235,102 @@ def alpha(SA,CT,p):
     return(alpha_return)
 
 
+@match_args_return
+def alpha_on_beta(SA,CT,p):
+
+    """
+    alpha_on_beta                          alpha/beta (76-term equation)
+    ====================================================================
+
+    USAGE:
+    alpha_on_beta = gsw.alpha_on_beta(SA,CT,p)
+
+    DESCRIPTION:
+    Calculates alpha divided by beta, where alpha is the thermal
+    expansion coefficient and beta is the saline contraction coefficient
+    of seawater from Absolute Salinity and Conservative Temperature.
+    This function uses the computationally-efficient expression for
+    specific volume in terms of SA, CT and p (Roquet et al., 2014).
+
+    Note that the 76-term equation has been fitted in a restricted range
+    of parameter space, and is most accurate inside the "oceanographic
+    funnel" described in IOC et al. (2010).  The GSW library function
+    "gsw.infunnel(SA,CT,p)" is avaialble to be used if one wants to test
+    if some of one's data lies outside this "funnel".
+
+    INPUT:
+    SA  =  Absolute Salinity                                    [ g/kg ]
+    CT  =  Conservative Temperature (ITS-90)                   [ deg C ]
+    p   =  sea pressure                                         [ dbar ]
+           ( i.e. absolute pressure - 10.1325 dbar )
+
+    SA & CT need to have the same dimensions.
+    p may have dimensions 1x1 or Mx1 or 1xN or MxN, where SA & CT are
+    MxN.
+
+    OUTPUT:
+    alpha_on_beta  =  thermal expansion coefficient with respect to
+                      Conservative Temperature divided by the saline
+                      contraction coefficient at constant Conservative
+                      Temperature                       [ kg g^-1 K^-1 ]
+
+    AUTHOR:
+    Paul Barker and Trevor McDougall                [ help@teos-10.org ]
+
+    VERSION NUMBER: 3.04 (10th December, 2013)
+
+    REFERENCES:
+    IOC, SCOR and IAPSO, 2010: The international thermodynamic equation
+    of seawater - 2010: Calculation and use of thermodynamic properties.
+    Intergovernmental Oceanographic Commission, Manuals and Guides No.
+    56, UNESCO (English), 196 pp.  Available from http://www.TEOS-10.org
+    See appendix A.20 and appendix K of this TEOS-10 Manual.
+
+    Roquet, F., G. Madec, T.J. McDougall, P.M. Barker, 2014: Accurate
+    polynomial expressions for the density and specifc volume of
+    seawater using the TEOS-10 standard. Ocean Modelling.
+
+    The software is available from http://www.TEOS-10.org
+    """
+    # This line ensures that SA is non-negative.
+    SA = np.maximum(SA, 0)
+
+    # deltaS = 24;
+    sfac = 0.0248826675584615             # sfac = 1/(40*(35.16504/35)).
+    offset = 5.971840214030754e-1                # offset = deltaS*sfac.
+
+    x2 = sfac*SA
+    xs = np.sqrt(x2 + offset)
+    ys = CT*0.025
+    z = p*1e-4
+
+    v_CT_part = ( a000 + xs*(a100 + xs*(a200 + xs*(a300 + xs*(a400
+        + a500*xs)))) + ys*(a010 + xs*(a110 + xs*(a210 + xs*(a310
+        + a410*xs))) + ys*(a020 + xs*(a120 + xs*(a220 + a320*xs))
+        + ys*(a030 + xs*(a130 + a230*xs) + ys*(a040 + a140*xs
+        + a050*ys )))) + z*(a001 + xs*(a101 + xs*(a201 + xs*(a301
+        + a401*xs))) + ys*(a011 + xs*(a111 + xs*(a211 + a311*xs))
+        + ys*(a021 + xs*(a121 + a221*xs) + ys*(a031 + a131*xs
+        + a041*ys))) + z*(a002 + xs*(a102 + xs*(a202 + a302*xs))
+        + ys*(a012 + xs*(a112 + a212*xs) + ys*(a022 + a122*xs
+        + a032*ys)) + z*(a003 + a103*xs + a013*ys + a004*z))) )
+
+    v_SA_part = ( b000 + xs*(b100 + xs*(b200 + xs*(b300 + xs*(b400
+        + b500*xs)))) + ys*(b010 + xs*(b110 + xs*(b210 + xs*(b310
+        + b410*xs))) + ys*(b020 + xs*(b120 + xs*(b220 + b320*xs))
+        + ys*(b030 + xs*(b130 + b230*xs) + ys*(b040 + b140*xs
+        + b050*ys)))) + z*(b001 + xs*(b101 + xs*(b201 + xs*(b301
+        + b401*xs))) + ys*(b011 + xs*(b111 + xs*(b211 + b311*xs))
+        + ys*(b021 + xs*(b121 + b221*xs) + ys*(b031 + b131*xs
+        + b041*ys))) + z*(b002 + xs*(b102 + xs*(b202 + b302*xs))
+        + ys*(b012 + xs*(b112 + b212*xs) + ys*(b022 + b122*xs
+        + b032*ys)) + z*(b003 +  b103*xs + b013*ys + b004*z))) )
+
+    alpha_on_beta_return = -(v_CT_part*xs)/(20*sfac*v_SA_part)
+
+    return(alpha_on_beta_return)
+
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
